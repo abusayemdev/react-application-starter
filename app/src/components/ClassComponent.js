f2d4884506401cd2d399bf35641c44ee3c13118d
abusayemdev <abusayem.devmail@gmail.com>
2025-02-12 02:18:50 +0600

    Functional Component, Class Component

---
+++ app/src/components/ClassComponent.js
@@ -0,0 +1,15 @@
+//------------------------- Class Component -----------------------
+//We extend class as component in Class Component
+//We have to use render() function in Class Component
+
+import React, { Component } from 'react'
+
+class ClassComponent extends Component{
+    render() {
+        return <h1>Hello World (Class Component)</h1>;
+    }
+    
+}
+
+
+export default ClassComponent 
---
+++ app/src/components/FunctionalComponent.js
@@ -0,0 +1,11 @@
+//---------------------- Functional Component -------------------------- 
+//We return a function in Functional Component
+
+import React from 'react'
+
+function FunctionalComponent(){
+    return <h1>Hello World (Functional Component)</h1>;
+}
+
+
+export default FunctionalComponent 
